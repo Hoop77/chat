3955951abc3cde0d0b28d45c47bc94870b1bad0e
Hoop77 <p.badenhoop@gmx.de>
2015-03-30 18:35:58 +0200

    added -set_status.php- and -remove_users_from_group.php-

--- webservice/remove_users_from_group.php
+++ webservice/remove_users_from_group.php
@@ -22,7 +22,7 @@ if(	!empty($_POST['active_username']) &&
 	if(!empty($not_existing_usernames)) {
 		// concat not existing usernames to one string
 		$str_not_existing_usernames = list_strings($not_existing_usernames);
-		response_error("Username(s) do not exist: " . $str_not_existing_usernames);
+		response_error("Username(s) do not exist in group: " . $str_not_existing_usernames);
 	}
 
 	// input chat id
@@ -42,6 +42,19 @@ if(	!empty($_POST['active_username']) &&
 		response_error("Error: Either not group or chat_id is not valid!");
 	}
 
+	$group_removed = false;
+
+	$query = "	SELECT username
+				FROM chats_has_users
+				INNER JOIN users ON user_id = id
+				WHERE chat_id = :chat_id";
+	$query_params = array(
+		':chat_id' => $chat_id
+	);
+
+	$stmt = exec_query($db, $query, $query_params, "Database Error3: Please try again!");
+	$users_in_group = $stmt->fetchAll();
+
 	$removed_usernames = array();
 	foreach ($remove_usernames as $i_remove_username => $remove_username) {
 		$query = "	SELECT 1
@@ -56,13 +69,15 @@ if(	!empty($_POST['active_username']) &&
 		$stmt = exec_query($db, $query, $query_params, "Database Error3: Please try again!");
 		$row = $stmt->fetch();
 		if($row) {
+			// add removed username to output list
 			array_push($removed_usernames, $remove_username);
+			// remove username from remaining remove list
 			if(($key = array_search($remove_username, $remove_usernames)) !== false) {
 				unset($remove_usernames[$key]);
 			}
 		}
 
-		$query = "	DELETE chats_has_users 
+		$query = "	DELETE chats_has_users
 					FROM chats_has_users
 					INNER JOIN users ON user_id = id
 					WHERE chat_id = :chat_id AND username = :remove_username";
@@ -72,11 +87,37 @@ if(	!empty($_POST['active_username']) &&
 		);
 
 		$stmt = exec_query($db, $query, $query_params, "Database Error3: Please try again!");
+
+		// see how many members are remaining in the group
+		$query = "	SELECT 1
+					FROM chats_has_users
+					WHERE chat_id = :chat_id";
+		$query_params = array(
+			':chat_id' => $chat_id
+		);
+
+		$stmt = exec_query($db, $query, $query_params, "Database Error4: Please try again!");			
+		$row = $stmt->fetchAll();
+
+		$len = count($row);
+		if($len == 0) {
+			$query = "	DELETE FROM chats
+						WHERE id = :chat_id";
+			$query_params = array(
+				':chat_id' => $chat_id
+			);						
+
+			$stmt = exec_query($db, $query, $query_params, "Database Error5: Please try again!");
+			$group_removed = true;
+		}
 	}
 
 	$result['removed_usernames'] = $removed_usernames;
 	$result['not_in_group_usernames'] = $remove_usernames;
-	response_success("Success! removed users: " . list_strings($removed_usernames) . " Not in group: " . list_strings($remove_usernames), 
+	$result['group_removed'] = $group_removed;
+	response_success("Success! removed users: " . list_strings($removed_usernames) 
+												. " not in group: " . list_strings($remove_usernames) 
+												. " group removed: " . ($group_removed ? "1" : "0"), 
 							"result", $result);
 }
 
